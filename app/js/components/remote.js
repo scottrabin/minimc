7c50fd3883c0f21ae4d2a2bd736b959589584337
Scott Rabin <scottrabin@gmail.com>
2013-02-03 15:26:02 -0800

    Added a grip handle to the remote header
    Dragging the grip up or down past a certain threshold (currently 50px)
    will cause the remote to show in full or collapse to just the header.

--- app/js/components/remote.js
+++ app/js/components/remote.js
@@ -5,6 +5,8 @@ define([
 	'js/services/player',
 ], function(defineComponent, Player) {
 
+	var DRAG_THRESHOLD = 50;
+
 	return defineComponent(remote);
 
 	function remote() {
@@ -13,6 +15,7 @@ define([
 			"selectorRewind" : ".rewind",
 			"selectorPlayPause" : ".play-pause",
 			"selectorFastForward" : ".forward",
+			"selectorGrip" : "#grippy-grip",
 		});
 
 		this.rewind = function() {
@@ -33,6 +36,49 @@ define([
 				toggleClass('icon-pause', speed === 1);
 		}
 
+		this.startDrag = function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+			if (!is_dragging(this)) {
+				this.$node.
+					addClass('dragging').
+					data({
+						'lastPosition' : get_y(event),
+						'startPosition' : get_y(event),
+					});
+
+				this.on(document, 'mousemove', this.drag);
+				this.on(document, 'touchmove', this.drag);
+				this.on(document, 'mouseup', this.stopDrag);
+				this.on(document, 'touchend', this.stopDrag);
+			}
+		};
+		this.stopDrag = function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+			if (is_dragging(this)) {
+				this.off(document, 'mousemove', this.drag);
+				this.off(document, 'touchmove', this.drag);
+				this.off(document, 'mouseup', this.stopDrag);
+				this.off(document, 'touchend', this.stopDrag);
+				this.$node.
+					removeClass('dragging').
+					css('height', this.$node.data('lastPosition') - this.$node.data('startPosition') > DRAG_THRESHOLD ? window.innerHeight : '');
+			}
+		};
+		this.drag = function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+
+			// if this would cause the remote height to drop below the minimum, ignore it
+			var newHeight = this.$node.height() + get_y(event) - this.$node.data('lastPosition');
+			if (newHeight > min_settable_height(this.$node)) {
+				this.$node.
+					height( newHeight ).
+					data('lastPosition', get_y(event));
+			}
+		};
+
 		this.after('initialize', function() {
 			this.on('click', {
 				"selectorRewind" : this.rewind,
@@ -41,6 +87,25 @@ define([
 			});
 
 			this.on(document, 'playerSpeedChanged', this.updateControl);
+
+			this.on('mousedown', {
+				"selectorGrip" : this.startDrag,
+			});
+			this.on('touchstart', {
+				"selectorGrip" : this.startDrag,
+			});
 		});
+	}
+
+	function get_y(event) {
+		return (event.originalEvent.touches ? event.originalEvent.touches[0].screenY : event.pageY);
+	}
+	function is_dragging(component) {
+		return component.$node.hasClass('dragging');
+	}
+	function min_settable_height($node) {
+		return _.reduce(['borderTopWidth', 'paddingTop', 'paddingBottom', 'borderBottomWidth'], function(memo, prop) {
+			return memo - parseFloat($node.css(prop));
+		}, parseFloat($node.css('minHeight')));
 	}
 });
